a0f4a12f047d72e362fb5259633ffa16019b6a14
Adam Ližičiar <adamliziciar27@gmail.com>
2022-03-11 11:38:32 +0100

    Basic program

---
+++ corona.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+###############################################
+# Name:     Corona - positive people analyzer #
+# Author:   Adam Ližičiar, xlizic00           #
+###############################################
+
+################# Functions ###################
+
+
+################### Main #######################
+export POSIXLY_CORRECT=yes
+
+if  [ "$1" = "-h" ] 
+then
+    echo "Hello world"
+fi
+
+exit 0
